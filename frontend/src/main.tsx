35bbd5dd94f0cfa362c545d4285254d17b18b8df
Matija Munjaković <mmunjako@gmail.com>
2026-07-12 21:58:05 +0200

    feat(frontend): add Readex Pro font and update UI tokens

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "@fontsource/readex-pro/400.css";
+import "@fontsource/readex-pro/500.css";
+import "@fontsource/readex-pro/600.css";
+import "@fontsource/readex-pro/700.css";
 import "./tokens.css";
 import "./shell.css";
 import "./tabs.css";
